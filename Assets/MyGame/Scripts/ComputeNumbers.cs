1f32a889c353f7e86585c16f293421c6b7300bc6
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2019-12-18 11:06:47 +0100

    Added unity object

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField ipfVarA;
+    public InputField ipfVarB;
+    public Button btnSubNumbers;
+    public Button btnReset;
+
+    void Start()
+    {
+        btnReset.interactable = false;
+    }
+
+    public void ResetOnClick()
+    {
+        ipfVarA.interactable = true;
+        ipfVarB.interactable = true;
+        btnSubNumbers.interactable = true;
+        btnReset.interactable = false;
+
+        result.text = "Result";
+        ipfVarA.text = "0";
+        ipfVarB.text = "0";
+    }
+
+    public void SubNumbersOnClick()
+    {
+        int variableA = int.Parse(ipfVarA.text);
+        int variableB = int.Parse(ipfVarB.text);
+        int subResult = variableA - variableB;
+        result.text = subResult.ToString();
+
+        ipfVarA.interactable = false;
+        ipfVarB.interactable = false;
+        btnSubNumbers.interactable = false;
+        btnReset.interactable = true;
+    }
+}
---
+++ Assets/MyGame/Scripts/Pythagoras.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pythagoras : MonoBehaviour
+{
+
+    public int NumberInput1;
+    public int NumberInput2;
+
+    public float a;
+    public float b;
+
+    private void Start()
+    {
+        //int result = Square(NumberInput1,NumberInput2) + Square(NumberInput1,NumberInput2);
+        //Debug.Log("Result Addition: " + result);
+        Pythogaras(a, b);
+    }
+
+    void OnValidate()
+    {
+        //Square(NumberInput1,NumberInput2);
+    }
+
+     int Square(int Num1, int Num2)
+    {
+        //Debug.Log ("Result Square: " + Num1*Num2);
+        return (Num1*Num2);
+    }
+
+    void Pythogaras(float a, float b)
+    {
+        float cSquare;
+        cSquare = (a * a + b * b);
+        Debug.Log("cSquare: " + cSquare);
+        float c = (Mathf.Sqrt(cSquare));
+        Debug.Log("c: " + (Mathf.Sqrt(cSquare)));
+    }
+}
